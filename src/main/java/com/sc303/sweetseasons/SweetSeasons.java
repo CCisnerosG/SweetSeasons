2be5e5c5822cad24ab460b32665f20916c257d47
Carlos Cisneros <116586023+CCisnerosG@users.noreply.github.com>
2023-10-04 21:42:30 -0600

    Primer commit

---
+++ src/main/java/com/sc303/sweetseasons/SweetSeasons.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sc303.sweetseasons;
+
+/**
+ *
+ * @author carlos
+ */
+public class SweetSeasons {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
