a9149083104b35ba39a97a9bb508c0bd2dde2865
Carlos Cisneros <116586023+CCisnerosG@users.noreply.github.com>
2023-11-12 15:23:49 -0600

    Prueba con ojdbc

---
+++ SweetSeasons/src/pruebadboracle/PruebaDBOracle.java
@@ -0,0 +1,43 @@
+package pruebadboracle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PruebaDBOracle {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error al cargar el controlador JDBC: " + e.getLocalizedMessage());
+            return;
+        }
+
+        String url = "jdbc:oracle:thin:@192.168.0.12:1521:orcldb";
+        String user = "HR";
+        String pass = "hr";
+        String sql = "SELECT EMPLOYEE_ID FROM EMPLOYEES WHERE LAST_NAME = 'King'";
+        int empId;
+        Connection conn;
+
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                empId = rs.getInt(1);
+                System.out.println("El código de empleado es: " + empId);
+            }
+            
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión: " + e.getLocalizedMessage());
+        }
+    }
+}
