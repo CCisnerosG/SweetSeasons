2bf4652ddc44832fa89df280666b9c9ceebb69a9
CisnerosJAVA <cabeto19851@gmail.com>
2023-12-04 20:12:19 -0600

    Correccion conexion BD Cisneros

--- SweetSeasons/src/pruebadboracle/ConexionBD.java
+++ SweetSeasons/src/pruebadboracle/ConexionBD.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pruebadboracle;
 
 import java.sql.Connection;
@@ -12,11 +8,12 @@ import javax.swing.JOptionPane;
 public class ConexionBD {
     private final String driver = "oracle.jdbc.driver.OracleDriver";
     
-    //private final String url = "jdbc:oracle:thin:@192.168.0.12:1521:orcldb"; //Ip MV Carlos 
+    private final String url = "jdbc:oracle:thin:@192.168.0.11:1521:ORCLDB"; //Ip MV Carlos 
     //private final String url = "jdbc:oracle:thin:@192.168.1.24:1521:orcldb"; //IP MV ANDY
-    private final String url = "jdbc:oracle:thin:@192.168.56.101:1521:orcl"; //Ip MV Ma Fer
+    //private final String url = "jdbc:oracle:thin:@192.168.56.101:1521:orcl"; //Ip MV Ma Fer
     private final String user = "ProyectoSS";
-    private final String pass = "OracleUwu";
+    //private final String pass = "OracleUwu"; //Mv Ma Fer
+    private final String pass = "1234"; //Mv Carlos
     
     public Connection cadena;
     
